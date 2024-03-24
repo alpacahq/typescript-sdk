8013a65755349cb3e7aae2ac5cf5582f5c256012
117 <16513382+117@users.noreply.github.com>
2024-03-23 20:27:27 -0600

    Refactor token bucket tests and add documentation

--- factory/createTokenBucket.test.ts
+++ factory/createTokenBucket.test.ts
@@ -1,27 +1,30 @@
 import { assert } from "https://deno.land/std@0.217.0/assert/assert.ts";
 import { createTokenBucket } from "./createTokenBucket.ts";
 
-Deno.test("Token bucket should allow taking tokens within capacity", () => {
+Deno.test("should allow taking tokens within capacity", () => {
   const tokenBucket = createTokenBucket({
     capacity: 200,
     fillRate: 3,
   });
+
   assert(tokenBucket.take(50) === true);
 });
 
-Deno.test("Token bucket should reject taking tokens beyond capacity", () => {
+Deno.test("should reject taking tokens beyond capacity", () => {
   const tokenBucket = createTokenBucket({
     capacity: 200,
     fillRate: 3,
   });
+
   assert(tokenBucket.take(300) === false);
 });
 
-Deno.test("Token bucket should refill tokens based on fill rate", async () => {
+Deno.test("should refill tokens based on fill rate", async () => {
   const tokenBucket = createTokenBucket({
     capacity: 200,
     fillRate: 3,
   });
+
   tokenBucket.take(50);
 
   await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -29,18 +32,16 @@ Deno.test("Token bucket should refill tokens based on fill rate", async () => {
   assert(tokenBucket.take(50) === true);
 });
 
-Deno.test(
-  "Token bucket should support 200 requests per 60 seconds",
-  async () => {
-    const tokenBucket = createTokenBucket({ capacity: 200, fillRate: 3 });
+Deno.test("should support 200 requests per 60 seconds", async () => {
+  const tokenBucket = createTokenBucket({ capacity: 200, fillRate: 3 });
 
-    let successfulRequests = 0;
-    for (let i = 0; i < 200; i++) {
-      if (tokenBucket.take(1)) {
-        successfulRequests++;
-      }
+  let successfulRequests = 0;
+
+  for (let i = 0; i < 200; i++) {
+    if (tokenBucket.take(1)) {
+      successfulRequests++;
     }
-
-    assert(successfulRequests === 200);
   }
-);
+
+  assert(successfulRequests === 200);
+});
--- factory/createTokenBucket.ts
+++ factory/createTokenBucket.ts
@@ -1,28 +1,55 @@
+/**
+ * Token Bucket
+ * @see https://en.wikipedia.org/wiki/Token_bucket
+ * @param {Object} options
+ * @param {number} options.capacity - maximum number of tokens
+ * @param {number} options.fillRate - tokens per second
+ */
 export type TokenBucketOptions = {
   capacity: number;
   fillRate: number;
 };
 
+/**
+ * Create a token bucket
+ * @param {TokenBucketOptions} options
+ * @returns {Object} token bucket
+ * @returns {number} token bucket.tokens - current number of tokens
+ * @returns {Function} token bucket.take - take tokens from the bucket
+ * @example
+ * const tokenBucket = createTokenBucket({ capacity: 200, fillRate: 3 });
+ *
+ * tokenBucket.take(10); // true
+ * tokenBucket.take(200); // false
+ *
+ * console.log(tokenBucket.tokens); // 190
+ */
 export const createTokenBucket = (
   { capacity, fillRate }: TokenBucketOptions = {
     capacity: 200,
     fillRate: 3,
   }
 ) => {
+  // Initialize tokens and last fill time
   let tokens = capacity;
+
+  // Initialize last fill time
   let lastFillTime = Date.now();
 
   return {
+    tokens,
     take: (count: number) => {
+      // Calculate time delta
       const now = Date.now();
       const delta = (now - lastFillTime) / 1000;
+
+      // Update last fill time and tokens
       lastFillTime = now;
       tokens = Math.min(capacity, tokens + delta * fillRate);
-      if (tokens < count) {
-        return false;
-      }
-      tokens -= count;
-      return true;
+
+      // Check if tokens are available and take them if so
+      // Otherwise, return false
+      return tokens >= count ? ((tokens -= count), true) : false;
     },
   };
 };
